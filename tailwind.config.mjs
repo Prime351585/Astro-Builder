3aa731c38264b7fdf75c4a55d2142969c4249577
Harsh Maurya <harsh03092002@outlook.com>
2024-08-25 17:28:10 +0000

    Final Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			  },
+			colors:{
+				primary: '#ff5525',
+				secondary: {
+					100: '#000000',
+					200: '#888883',
+				}
+			},
+			animation:{
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+			},
+			keyframes: {
+			  'infinite-scroll': {
+				from: { transform: 'translateX(0)' },
+				to: { transform: 'translateX(-100%)' },
+			  }
+			}
+			}
+		},
 	plugins: [],
 }
