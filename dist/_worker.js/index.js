328733b9d2f52ea8712f683b507098d2fdb5028d
Prime351585 <harsh03092002@outlook.com>
2025-11-25 21:30:38 +0530

    CHanged Images to AVIF

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CyqVHSlY.mjs';
-import { manifest } from './manifest_Mm2cYd8P.mjs';
+import { manifest } from './manifest_P0FoCjwv.mjs';
 
 const serverIslandMap = new Map();;
 
