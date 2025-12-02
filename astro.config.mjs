8bf314bf931a1c1a9e39fe4a31d9e46fbf8a059e
Prime351585 <harsh03092002@outlook.com>
2025-12-02 12:49:35 +0530

    Make some Speed Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   // output: "server",
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
   site:"https://dwizzyinfra.com/",
  
   
