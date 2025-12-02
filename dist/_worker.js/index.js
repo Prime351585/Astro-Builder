3f8610c850be21a390f29d9926a681ce77bb21a2
Prime351585 <harsh03092002@outlook.com>
2025-12-02 12:34:43 +0530

    Added Robots.txt Added Image Optimizations Added Inline Fonts

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CyqVHSlY.mjs';
-import { manifest } from './manifest_EsVCw192.mjs';
+import { manifest } from './manifest_OpYY0NG1.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -10,20 +10,22 @@ const _page1 = () => import('./pages/certifications.astro.mjs');
 const _page2 = () => import('./pages/contact.astro.mjs');
 const _page3 = () => import('./pages/privacypolicy.astro.mjs');
 const _page4 = () => import('./pages/projects.astro.mjs');
-const _page5 = () => import('./pages/services/pile-foundation.astro.mjs');
-const _page6 = () => import('./pages/services/power-transmission.astro.mjs');
-const _page7 = () => import('./pages/services.astro.mjs');
-const _page8 = () => import('./pages/index.astro.mjs');
+const _page5 = () => import('./pages/robots.txt.astro.mjs');
+const _page6 = () => import('./pages/services/pile-foundation.astro.mjs');
+const _page7 = () => import('./pages/services/power-transmission.astro.mjs');
+const _page8 = () => import('./pages/services.astro.mjs');
+const _page9 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/about.astro", _page0],
     ["src/pages/certifications.astro", _page1],
     ["src/pages/contact.astro", _page2],
     ["src/pages/privacyPolicy.astro", _page3],
     ["src/pages/projects.astro", _page4],
-    ["src/pages/services/pile-foundation.astro", _page5],
-    ["src/pages/services/power-transmission.astro", _page6],
-    ["src/pages/services/index.astro", _page7],
-    ["src/pages/index.astro", _page8]
+    ["src/pages/robots.txt.ts", _page5],
+    ["src/pages/services/pile-foundation.astro", _page6],
+    ["src/pages/services/power-transmission.astro", _page7],
+    ["src/pages/services/index.astro", _page8],
+    ["src/pages/index.astro", _page9]
 ]);
 
 const _manifest = Object.assign(manifest, {
