7913394ff4f8e4ec480f7806036ce876a27029a9
Prime351585 <harsh03092002@outlook.com>
2025-12-02 12:57:28 +0530

    Changed Few Images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  // output: "server",
-  // adapter: cloudflare(),
   site:"https://dwizzyinfra.com/",
  
   
